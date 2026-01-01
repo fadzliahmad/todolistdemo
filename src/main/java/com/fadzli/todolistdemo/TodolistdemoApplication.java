4b0c37c6ccb15c4b3106b8fb0dc006910dd938b2
ahmadfadzli <ahmadfadzli@hasil.gov.my>
2026-01-01 13:06:08 +0800

    comment

--- src/main/java/com/fadzli/todolistdemo/TodolistdemoApplication.java
+++ src/main/java/com/fadzli/todolistdemo/TodolistdemoApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TodolistdemoApplication {
 
     public static void main(String[] args) {
+        //update
         SpringApplication.run(TodolistdemoApplication.class, args);
     }
 
