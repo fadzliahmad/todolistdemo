5c20e594ab4269088b617b519f9940c3394321d1
ahmadfadzli <ahmadfadzli@hasil.gov.my>
2026-01-01 13:02:24 +0800

    initial commit

---
+++ src/main/java/com/fadzli/todolistdemo/TodolistdemoApplication.java
@@ -0,0 +1,13 @@
+package com.fadzli.todolistdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodolistdemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TodolistdemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fadzli/todolistdemo/TodolistdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fadzli.todolistdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistdemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
