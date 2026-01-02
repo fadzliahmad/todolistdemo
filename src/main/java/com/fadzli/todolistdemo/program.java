1ca2f7d292ca123e8ce34b6315cf9761bc786718
ahmadfadzli <ahmadfadzli@hasil.gov.my>
2026-01-02 11:49:48 +0800

    setup: added mysql driver dependencies and setup database connection strings

--- src/main/java/com/fadzli/todolistdemo/program.java
+++ src/main/java/com/fadzli/todolistdemo/program.java
@@ -4,11 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TodolistdemoApplication {
+public class program {
 
     public static void main(String[] args) {
-        //update
-        SpringApplication.run(TodolistdemoApplication.class, args);
+        SpringApplication.run(program.class, args);
     }
 
 }
--- src/test/java/com/fadzli/todolistdemo/programTests.java
+++ src/test/java/com/fadzli/todolistdemo/programTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class TodolistdemoApplicationTests {
+class programTests {
 
     @Test
     void contextLoads() {
